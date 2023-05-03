ef9c2f8ddb8e335d4985eaef5517a2cdf1a264bc
Vincent Markham <vincent.markham@gmail.com>
2023-05-03 11:57:03 -0700

    Tie the pieces together and use Auth to protect notes. Add tailwind styling.

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,50 +1,69 @@
 // src/components/NoteList.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getNotes, createNote, NoteData } from "../noteAPI";
+import useAuth from "../hooks/useAuth";
+import CreateNote from "./CreateNote";
+import CollapsibleNote from "./CollapsibleNote";
+import { Timestamp } from "firebase/firestore";
 
 interface Note {
   id: string;
   title: string;
   content: string;
-  createdAt: Date;
+  createdAt: Timestamp;
 }
 
-// Sample notes
-const sampleNotes: Note[] = [
-  {
-    id: "1",
-    title: "Sample Note 1",
-    content: "This is the description of the Sample Note 1.",
-    createdAt: new Date(),
-  },
-  {
-    id: "2",
-    title: "Sample Note 2",
-    content: "This is the description of the Sample Note 2.",
-    createdAt: new Date(),
-  },
-];
-
 const NoteList: React.FC = () => {
-  const [notes, setNotes] = useState(sampleNotes);
+  const [notes, setNotes] = useState<NoteData[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
+
+  const user = useAuth();
+  const uid = user?.uid || "";
+
+  useEffect(() => {
+    if (uid === "") return;
+
+    const fetchNotes = async () => {
+      console.log("fetchNotes");
+      const userNotes = await getNotes(uid);
+      console.log("userNotes");
+      console.log(userNotes);
+      setNotes(userNotes);
+    };
+
+    fetchNotes();
+  }, []);
+
+  const handleNoteCreated = (newNote: NoteData) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
 
   const filteredNotes = notes.filter((note) =>
     note.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search notes"
-      />
-      <ul>
+    <div className="bg-gray-100 min-h-screen p-6">
+      <CreateNote onNoteCreated={handleNoteCreated} uid={uid} />
+      <div className="w-full md:w-1/2">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search notes"
+          className="mt-4 w-full p-2 border border-gray-300 rounded"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {filteredNotes.map((note) => (
-          <li key={note.id}>{note.title}</li>
+          <CollapsibleNote
+            key={note.id}
+            title={note.title}
+            content={note.content}
+            createdAt={note.createdAt} // Pass the createdAt prop
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
